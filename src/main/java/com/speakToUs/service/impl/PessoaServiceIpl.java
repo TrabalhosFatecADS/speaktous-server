20e1f89debf54e1ea441d2d45b4381162507ac8e
Douglas <douglas.ribeiro.id@gmail.com>
2019-05-24 16:44:43 -0300

    Commit 17/05/2019

--- src/main/java/com/speakToUs/service/impl/PessoaServiceIpl.java
+++ src/main/java/com/speakToUs/service/impl/PessoaServiceIpl.java
@@ -1,10 +1,13 @@
 package com.speakToUs.service.impl;
 
 import com.speakToUs.dao.PessoaDao;
+import com.speakToUs.dao.UserDao;
 import com.speakToUs.model.Pessoa;
+import com.speakToUs.model.User;
 import com.speakToUs.service.PessoaService;
 import com.speakToUs.util.GerarSenha;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
@@ -17,15 +20,30 @@ import java.util.Date;
 public class PessoaServiceIpl implements PessoaService {
 
     @Autowired
+    private BCryptPasswordEncoder bcryptEncoder;
+
+    @Autowired
     private PessoaDao pessoaDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Pessoa save(Pessoa pessoa) {
         if (pessoaDao.findPessoaByEmail(pessoa.getEmail()) == null) {
+            String senha = pessoa.getSenha();
             pessoa.setSenha(GerarSenha.senhaMD5(pessoa.getSenha()));
             Date dtToday = new Date();
             pessoa.setDtCadastro(new Date());
             pessoaDao.save(pessoa);
+
+
+            User newUser = new User();
+            newUser.setPassword(bcryptEncoder.encode(senha));
+            newUser.setUsername(pessoa.getEmail());
+            userDao.save(newUser);
+
+
             return pessoa;
         }else{
             return null;
